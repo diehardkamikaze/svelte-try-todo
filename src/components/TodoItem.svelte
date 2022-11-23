242267fbd24c6c8a81d83094c9d1da95f732f595
captainAlatriste <diehardkamikaze@gmail.com>
2022-11-23 04:57:54 +0300

    todo realized

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let todowka = "";
+  export let index = "";
+
+  const deleteTodoItem = () => {
+    dispatch("delete", index);
+  };
+</script>
+
+<li>
+  <span>{index + 1}: {todowka}</span><a href="#" on:click={deleteTodoItem}
+    >Delete</a
+  >
+</li>
+
+<style>
+  li {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px;
+    font-weight: bold;
+    font-size: 18px;
+    background-color: slateblue;
+    color: white;
+  }
+
+  li a {
+    color: white;
+  }
+</style>
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,60 @@
+<script>
+  // @ts-nocheck
+  import TodoItem from "./TodoItem.svelte";
+
+  let newTodo = "";
+  let todos = [];
+
+  const addTodoEnter = function (event) {
+    if (event.code === "Enter") addTodo();
+  };
+
+  const addTodo = () => {
+    if (newTodo) {
+      todos.push(newTodo);
+      todos = todos;
+      newTodo = "";
+    }
+  };
+
+  const deleteTodobyIndex = (event) => {
+    todos.splice(event.detail, 1);
+    todos = todos;
+  };
+</script>
+
+<main>
+  <div class="tl">
+    <label for="todo-input">Новое дело:</label>
+    <input
+      on:keydown={addTodoEnter}
+      bind:value={newTodo}
+      id="todo-inpup"
+      type="text"
+      name="todo-input"
+      placeholder="Купить хлеба..."
+    />
+    <button on:click={addTodo}>Добавить</button>
+    <ul class="list">
+      {#each todos as todowka, i}
+        <TodoItem on:delete={deleteTodobyIndex} {todowka} index={i} />
+      {/each}
+    </ul>
+  </div>
+</main>
+
+<style>
+  .tl {
+    text-align: left;
+    width: 450px;
+    margin: auto;
+  }
+
+  .tl .list {
+    list-style-type: none;
+    border: 2px solid grey;
+    border-radius: 5%;
+    padding: 0;
+    margin-top: 10px;
+  }
+</style>
